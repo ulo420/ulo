87c803ad01f1338e478e9ab8606257492bcfd438
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2020-02-03 18:48:25 +0100

    zmiany wyglądu i kilku innych rzeczy
    Przeniosłem css i javascript do plików style.css i modulo.js, żeby odzielić html od innych elementów. Zastąpiłem definiowanie pozycjonowania elementów tabelami na divy i listy stylizowane cssem.

---
+++ modulo.js
@@ -0,0 +1,9 @@
+function calculatemodULO(){
+	
+	var a = document.getElementById("in1").value;
+	var b = document.getElementById("in2").value;
+
+	var c = a % b;
+
+	document.getElementById("wynikmodULO").innerHTML = c;
+}
