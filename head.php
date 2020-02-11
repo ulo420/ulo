49124b7710f5babd66c4c6c373c4e13a5c072ea3
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2020-02-11 19:46:14 +0100

    poprawki responsywności

--- head.php
+++ head.php
@@ -2,6 +2,8 @@
 
 echo '<meta charset="utf-8">
 <title>strona, najlepsza, bardzo, jak najbardziej jak kazał mi wpisać Igor</title>
-<link rel="stylesheet" type="text/css" href="style.css">';
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+';
 
 ?>
